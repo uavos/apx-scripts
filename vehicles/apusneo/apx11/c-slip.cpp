ef5f2154bb55677530c5d6dd332c62f3dd92e0f7
Slava Vasyukovich <vs@uavos.com>
2025-06-09 11:37:49 +0300

    feat: ultrasonik air sensor

--- vehicles/apusneo/apx11/c-slip.cpp
+++ vehicles/apusneo/apx11/c-slip.cpp
@@ -3,7 +3,22 @@
 constexpr const uint8_t TASK_AGL_MS{100};
 
 constexpr const port_id_t PORT_ID_AGL{5};
+constexpr const port_id_t PORT_ID_AIR{6};
 
+//AIR
+uint8_t MSG_AIR_SIZE = 35;
+uint8_t AIR_DATA[35] = {};
+
+float air_spd = 0.f;
+float air_hdg = 0.f;
+float air_state = 0.f;
+float air_temp = 0.f;
+
+using m_spd = Mandala<mandala::est::env::usr::u5>; //spd
+using m_hdg = Mandala<mandala::est::env::usr::u6>; //hdg
+using m_tmp = Mandala<mandala::est::env::usr::u7>; //temp
+
+//AGL
 using m_agl = Mandala<mandala::sns::nav::agl::laser>; //agl
 
 int main()
@@ -11,8 +26,15 @@ int main()
     task("on_task_agl", TASK_AGL_MS);
 
     receive(PORT_ID_AGL, "aglHandler");
+    receive(PORT_ID_AIR, "airHandler");
 
     return 0;
+}
+
+EXPORT void on_task_agl()
+{
+    //agl
+    send(PORT_ID_AGL, (const uint8_t *) "?LD\r\n", 5, true);
 }
 
 //ASCII:ld,0:-0.66
@@ -52,8 +74,76 @@ EXPORT void aglHandler(const uint8_t *data, size_t size)
     }
 }
 
-EXPORT void on_task_agl()
+//$WI,WVC=000.0,137,0,+027.4,C,A*4C
+EXPORT void airHandler(const uint8_t *data, size_t size)
 {
-    //agl
-    send(PORT_ID_AGL, (const uint8_t *) "?LD\r\n", 5, true);
+    if (size != MSG_AIR_SIZE) {
+        return;
+    }
+
+    memcpy(&AIR_DATA, data, MSG_AIR_SIZE);
+
+    if (AIR_DATA[0] != '$' || AIR_DATA[1] != 'W' || AIR_DATA[2] != 'I') {
+        return;
+    }
+
+    uint8_t indx = 0;
+    uint8_t v[4] = {};
+
+    //---
+    while (AIR_DATA[indx] != 0x2C) {
+        indx++;
+    }
+
+    v[0] = AIR_DATA[indx + 5] - '0';
+    v[1] = AIR_DATA[indx + 6] - '0';
+    v[2] = AIR_DATA[indx + 7] - '0';
+    v[3] = AIR_DATA[indx + 9] - '0';
+
+    air_spd = v[0] * 100 + v[1] * 10 + v[2] * 1 + v[3] * 0.1f;
+
+    //---
+    indx++;
+    while (AIR_DATA[indx] != 0x2C) {
+        indx++;
+    }
+
+    v[0] = AIR_DATA[indx + 1] - '0';
+    v[1] = AIR_DATA[indx + 2] - '0';
+    v[2] = AIR_DATA[indx + 3] - '0';
+
+    air_hdg = v[0] * 100 + v[1] * 10 + v[2];
+
+    //---
+    indx++;
+    while (AIR_DATA[indx] != 0x2C) {
+        indx++;
+    }
+
+    air_state = AIR_DATA[indx + 1] - '0';
+
+    //---
+    indx++;
+    while (AIR_DATA[indx] != 0x2C) {
+        indx++;
+    }
+    v[0] = AIR_DATA[indx + 2] - '0';
+    v[1] = AIR_DATA[indx + 3] - '0';
+    v[2] = AIR_DATA[indx + 4] - '0';
+    v[3] = AIR_DATA[indx + 6] - '0';
+
+    air_temp = v[0] * 100 + v[1] * 10 + v[2] * 1 + v[3] * 0.1f;
+
+    if (AIR_DATA[indx + 1] == '-') {
+        air_temp = -air_temp;
+    }
+
+    //printf("air:%.2f", air_spd);
+    //printf("hdg:%.2f", air_hdg);
+    //printf("state:%.1f", air_state);
+    //printf("temp:%.2f", air_temp);
+
+    m_spd::publish(air_spd);
+    m_hdg::publish(air_hdg);
+    m_tmp::publish(air_temp);
 }
