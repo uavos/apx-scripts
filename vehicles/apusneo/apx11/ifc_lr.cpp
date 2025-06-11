4633360cee7f944f62aa56c9ed6b383c50fb9749
Slava Vasyukovich <vs@uavos.com>
2025-06-11 12:35:20 +0300

    upd: commands for subsystems

--- vehicles/apusneo/apx11/ifc_lr.cpp
+++ vehicles/apusneo/apx11/ifc_lr.cpp
@@ -16,55 +16,59 @@ using m_pump_rpm = Mandala<mandala::est::env::usrw::w2>; //pump rpm right
 #endif
 
 constexpr const uint8_t TASK_MAIN_MS{50}; //20Hz
-//constexpr const uint8_t TASK_SAVE_MANDALA_MS{100}; //100Hz
-
-//--------------------------size---------------------------
-constexpr const uint8_t PACK_CAN_SIZE{12};
-//---------------------------------------------------------
 
 //--------------------------pu-----------------------------
 constexpr const uint16_t PU_ID{128};
 constexpr const uint16_t PU_SHIFT{20};
-//constexpr const uint16_t PU_CMD_HEATER{PU_ID + 10};
+constexpr const uint16_t PU_CMD_HEATER{PU_ID + 10};
 constexpr const uint16_t PU_CMD_ON{PU_ID + 12};
 constexpr const uint16_t PU_CMD_RB{PU_ID + 13};
-//---------------------------------------------------------
 
 //--------------------------sp-----------------------------
-//constexpr const uint16_t SP_ID{512};
-//constexpr const uint16_t SP_SHIFT{20};
-//constexpr const uint16_t SP_CMD_POWER_ON{SP_ID + 10};
+constexpr const uint16_t SP_ID{512};
+constexpr const uint16_t SP_SHIFT{20};
+constexpr const uint16_t SP_CMD_POWER_ON{SP_ID + 10};
 
-//bool sp_state{false};
-//---------------------------------------------------------
-
+//--------------------------pump---------------------------
 using m_pump_rpm_raw = Mandala<mandala::sns::env::scr::s5>; //pump rpm left/right
 uint32_t timer_changed_rpm{};
 constexpr const uint16_t PUMP_RPM_TIMEOUT{2000};
 
-//--------------------------Mandala------------------------
-//cmd
-using m_sp_pwr_ign = Mandala<mandala::ctr::env::pwr::eng>; //sp
-
 int main()
 {
-    m_sp_pwr_ign();
     m_pump_rpm_raw("on_pump_rpm"); // subscribe `on changed` event
 
-    task("pu_on");  //GCS with terminal command `vmexec("pu_on")`
-    task("pu_off"); //GCS with terminal command `vmexec("pu_off")`
+    task("pu");    //GCS with terminal command `vmexec("pu", 0)` or `vmexec("pu", 1)`
+    task("pu_rb"); //GCS with terminal command `vmexec("pu_rb", 0)` or `vmexec("pu_rb", 100)`
 
-    task("pu_rb");
+#if defined NODE_LEFT
+    task("sp_1"); //GCS with terminal command `vmexec("sp_1",0)`  or  `vmexec("sp_1",1)`
+    task("sp_2"); //GCS with terminal command `vmexec("sp_2",0)`  or  `vmexec("sp_2",1)`
 
-    //task("sp_on");  //GCS with terminal command `vmexec("sp_on")`
-    //task("sp_off"); //GCS with terminal command `vmexec("sp_off")`
-    //m_pu_cmd_hpwr("on_cmd_hpwr");
+    task("pu_h1"); //GCS with terminal command `vmexec("pu_h1",0)`  or  `vmexec("pu_h1",30)`
+    task("pu_h2"); //GCS with terminal command `vmexec("pu_h2",0)`  or  `vmexec("pu_h2",30)`
+#endif
+
+#if defined NODE_RIGHT
+    task("sp_3"); //GCS with terminal command `vmexec("sp_3",0)`  or  `vmexec("sp_3",1)`
+    task("sp_4"); //GCS with terminal command `vmexec("sp_4",0)`  or  `vmexec("sp_4",1)`
+
+    task("pu_h3"); //GCS with terminal command `vmexec("pu_h3",0)`  or  `vmexec("pu_h3",30)`
+    task("pu_h4"); //GCS with terminal command `vmexec("pu_h4",0)`  or  `vmexec("pu_h4",30)`
+#endif
+
     task("on_main", TASK_MAIN_MS); //20 Hz
-    //task("on_save_mandala", TASK_SAVE_MANDALA_MS); //10 Hz
 
     printf("IFC:%s Script ready...\n", txt_dev);
 
     return 0;
+}
+
+EXPORT void on_main()
+{
+    if (time_ms() - timer_changed_rpm > PUMP_RPM_TIMEOUT) {
+        m_pump_rpm::publish(0u);
+    }
 }
 
 EXPORT void on_pump_rpm()
@@ -83,8 +87,10 @@ T limit(T value, T min, T max)
     return value;
 }
 
-EXPORT void sendCmdToCan(const uint32_t &can_id, const uint8_t *data, const uint8_t &size)
+void sendCmdToCan(const uint32_t &can_id, const uint8_t *data, const uint8_t &size)
 {
+    constexpr const uint8_t PACK_CAN_SIZE{12};
+
     uint8_t msg[PACK_CAN_SIZE] = {};
 
     msg[0] = (uint8_t) can_id;         //ID_0_7
@@ -119,57 +125,27 @@ void pu_cmd_rb(uint8_t val)
     sendCmdToCan(PU_CMD_RB + PU_SHIFT, msg, 1);
 }
 
-//void pu_cmd_heater(const uint8_t &dev_id, const uint8_t &value)
-//{
-//    const uint16_t cmd = value * 100;
-//
-//    uint8_t msg[2] = {};
-//    msg[0] = (uint8_t) cmd;
-//    msg[1] = cmd >> 8;
-//
-//    sendCmdToCan(PU_CMD_HEATER + dev_id * PU_SHIFT, msg, 2);
-//}
-
-//void sp_cmd_power_on(const uint8_t &value)
-//{
-//    uint8_t msg[1] = {value};
-//
-//    sendCmdToCan(SP_CMD_POWER_ON, msg, 1);
-//    sendCmdToCan(SP_CMD_POWER_ON + SP_SHIFT, msg, 1);
-//}
-
-EXPORT void on_main()
+void pu_cmd_heater(const uint8_t &dev_id, const uint8_t &value)
 {
-    //    bool pwr_ign = (bool) m_sp_pwr_ign::value();
-    //    if (sp_state != pwr_ign) {
-    //        sp_cmd_power_on(pwr_ign);
-    //        sp_state = pwr_ign;
-    //    }
+    const uint16_t cmd = value * 100;
 
-    if (time_ms() - timer_changed_rpm > PUMP_RPM_TIMEOUT) {
-        m_pump_rpm::publish(0u);
-    }
+    uint8_t msg[2] = {};
+    msg[0] = (uint8_t) cmd;
+    msg[1] = cmd >> 8;
+
+    sendCmdToCan(PU_CMD_HEATER + dev_id * PU_SHIFT, msg, 2);
 }
 
-//EXPORT void on_cmd_hpwr()
-//{
-//    constexpr const uint8_t PU_MIN_HEATER{0};
-//    constexpr const uint8_t PU_MAX_HEATER{30};
-//    uint8_t cmd = limit((uint8_t) m_pu_cmd_hpwr::value(), PU_MIN_HEATER, PU_MAX_HEATER);
-//    printf("cmd_hpwr:%u", cmd);
-//    pu_cmd_heater(1, cmd);
-//}
-
-EXPORT void pu_on()
+void sp_power(const uint8_t &dev_id, const uint8_t &value)
 {
-    printf("IFC-%s, pu_on...\n", txt_dev);
-    pu_cmd_power_on(1u);
+    uint8_t msg[1] = {value};
+    sendCmdToCan(SP_CMD_POWER_ON + dev_id * SP_SHIFT, msg, 1);
 }
 
-EXPORT void pu_off()
+EXPORT void pu(int32_t val)
 {
-    printf("IFC-%s, pu_off...\n", txt_dev);
-    pu_cmd_power_on(0u);
+    printf("IFC-%s, pu...\n", txt_dev);
+    pu_cmd_power_on((uint8_t) val);
 }
 
 EXPORT void pu_rb(int32_t val)
@@ -179,14 +155,54 @@ EXPORT void pu_rb(int32_t val)
     pu_cmd_rb((uint8_t) val);
 }
 
-//EXPORT void sp_on()
-//{
-//    printf("IFC-%s, sp_on...\n", txt_dev);
-//    sp_cmd_power_on(1u);
-//}
+#if defined NODE_LEFT
+EXPORT void sp_1(int32_t val)
+{
+    printf("IFC-%s, sp_1...\n", txt_dev);
+    sp_power(0, (uint8_t) val);
+}
 
-//EXPORT void sp_off()
-//{
-//    printf("IFC-%s, sp_off...\n", txt_dev);
-//    sp_cmd_power_on(0u);
-//}
+EXPORT void sp_2(int32_t val)
+{
+    printf("IFC-%s, sp_2...\n", txt_dev);
+    sp_power(1, (uint8_t) val);
+}
+
+EXPORT void pu_h1(int32_t val)
+{
+    printf("IFC-%s, pu_h1...\n", txt_dev);
+    pu_cmd_heater(0, (uint8_t) val);
+}
+
+EXPORT void pu_h2(int32_t val)
+{
+    printf("IFC-%s, pu_h2...\n", txt_dev);
+    pu_cmd_heater(1, (uint8_t) val);
+}
+#endif
+
+#if defined NODE_RIGHT
+EXPORT void sp_3(int32_t val)
+{
+    printf("IFC-%s, sp_3...\n", txt_dev);
+    sp_power(0, (uint8_t) val);
+}
+
+EXPORT void sp_4(int32_t val)
+{
+    printf("IFC-%s, sp_4...\n", txt_dev);
+    sp_power(1, (uint8_t) val);
+}
+
+EXPORT void pu_h3(int32_t val)
+{
+    printf("IFC-%s, pu_h3...\n", txt_dev);
+    pu_cmd_heater(0, (uint8_t) val);
+}
+
+EXPORT void pu_h4(int32_t val)
+{
+    printf("IFC-%s, pu_h4...\n", txt_dev);
+    pu_cmd_heater(1, (uint8_t) val);
+}
+#endif
