b5f8d85b18012b60734a93c7354f896a837c204e
Slava Vasyukovich <vs@uavos.com>
2025-07-24 11:12:43 +0400

    feat: status for heater

--- vehicles/apusneo/apx11/nav_cl.cpp
+++ vehicles/apusneo/apx11/nav_cl.cpp
@@ -2,15 +2,23 @@
 
 constexpr const char *txt_dev = "CL"; //CL
 
-constexpr const uint8_t TASK_MAIN_MS{100};
+constexpr const uint8_t TASK_MAIN_MS{500};
 constexpr const uint16_t TASK_HEATER_MS{2000};
 
 //----------------------Temperature-------------------------
 constexpr const uint16_t START_HEATER{10};
 
 using m_sw_manual = Mandala<mandala::ctr::env::sw::sw15>; //heater manual
+using m_status_heater = Mandala<mandala::est::env::usrc::c15>;
 
-using m_sw = Mandala<mandala::ctr::env::sw::sw2>;          //heater off/on
+using m_sw1 = Mandala<mandala::ctr::env::sw::sw1>;
+using m_sw2 = Mandala<mandala::ctr::env::sw::sw2>; //heater for main nav off/on
+using m_sw3 = Mandala<mandala::ctr::env::sw::sw3>;
+using m_sw4 = Mandala<mandala::ctr::env::sw::sw4>;
+using m_sw5 = Mandala<mandala::ctr::env::sw::sw5>;
+using m_sw6 = Mandala<mandala::ctr::env::sw::sw6>;
+using m_sw7 = Mandala<mandala::ctr::env::sw::sw7>;
+
 using m_sns_temp = Mandala<mandala::sns::nav::gyro::temp>; //gyro temp
 
 //get
@@ -21,6 +29,17 @@ using m_temp = Mandala<mandala::est::env::usrc::c7>;
 
 int main()
 {
+    //subscribe
+    m_sw1();
+    m_sw2();
+    m_sw3();
+    m_sw4();
+    m_sw5();
+    m_sw6();
+    m_sw7();
+
+    m_status_heater();
+
     m_s1();
 
     m_sns_temp();
@@ -38,6 +57,12 @@ int main()
 EXPORT void on_main()
 {
     m_temp::publish(m_s1::value() + 100);
+
+    uint8_t status = (m_sw1::value() & 0x01) | (m_sw2::value() & 0x02) | (m_sw3::value() & 0x04)
+                     | (m_sw4::value() & 0x08) | (m_sw5::value() & 0x10) | (m_sw6::value() & 0x20)
+                     | (m_sw7::value() & 0x40);
+
+    m_status_heater::publish(status);
 }
 
 EXPORT void on_heater()
@@ -49,10 +74,10 @@ EXPORT void on_heater()
     float RT = m_sns_temp::value();
 
     if (RT < START_HEATER) {
-        m_sw::publish(1u);
+        m_sw2::publish(1u);
     }
 
     if (RT > START_HEATER * 1.5f) {
-        m_sw::publish(0u);
+        m_sw2::publish(0u);
     }
 }
