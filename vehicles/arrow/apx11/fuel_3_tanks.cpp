9593f864877d11da4efc82de54e48a0123c7f36a
Slava Vasyukovich <vs@uavos.com>
2025-09-01 06:35:51 +0300

    fix: code style

--- vehicles/arrow/apx11/fuel_3_tanks.cpp
+++ vehicles/arrow/apx11/fuel_3_tanks.cpp
@@ -12,28 +12,27 @@ const uint8_t ADR_SENS3{77};
 const uint8_t PACK_SIZE{9};
 
 #ifdef EQUAL_TANKS
-    const float V_MAX1{16.7f};      //liters
-    const float V_MAX2{16.7f};      //liters
-    const float V_MAX3{16.7f};      //liters
+const float V_MAX1{16.7f}; //liters
+const float V_MAX2{16.7f}; //liters
+const float V_MAX3{16.7f}; //liters
 
-    const float CRITICAL_LOW{7.0f};   //7% = 1.1l
-    const float TANK_1_POINT{13.0f};  //13% = 2l
-    const float TANK_2_POINT{25.0f};  //25% = 3.9l
-    const float TANK_3_POINT{51.0f};  //51% = 8l
+const float CRITICAL_LOW{7.0f};  //7% = 1.1l
+const float TANK_1_POINT{13.0f}; //13% = 2l
+const float TANK_2_POINT{25.0f}; //25% = 3.9l
+const float TANK_3_POINT{51.0f}; //51% = 8l
 #else
-    const float V_MAX1{17.0f};      //liters
-    const float V_MAX2{18.3f};      //liters
-    const float V_MAX3{24.6f};      //liters
+const float V_MAX1{17.0f}; //liters
+const float V_MAX2{18.3f}; //liters
+const float V_MAX3{24.6f}; //liters
 
-    const float CRITICAL_LOW{7.0f};   //7% = 1.1l
-    const float TANK_1_POINT{20.0f};  //20% = 3.4l
-    const float TANK_2_POINT{25.0f};  //25% = 4.5l
-    const float TANK_3_POINT{60.0f};  //60% = 14.7l
+const float CRITICAL_LOW{7.0f};  //7% = 1.1l
+const float TANK_1_POINT{20.0f}; //20% = 3.4l
+const float TANK_2_POINT{25.0f}; //25% = 4.5l
+const float TANK_3_POINT{60.0f}; //60% = 14.7l
 #endif
 
-
-const uint8_t TIME_SA{2};       //sec
-const uint8_t DELAY_PUMP{15}; //sec between different pumps
+const uint8_t TIME_SA{2};             //sec
+const uint8_t DELAY_PUMP{15};         //sec between different pumps
 const uint16_t FUEL_TASK_PERIOD{200}; //msec
 
 uint8_t msg[PACK_SIZE];
@@ -52,7 +51,7 @@ uint32_t ignitionTotalTime{0};
 uint8_t pump_stage{1}; //default stage
 
 bool ers;
-bool algorithm;   //0 - enable; 1 - disable
+bool algorithm; //0 - enable; 1 - disable
 bool algorithmOld;
 bool ignition;
 bool ignitionOld;
@@ -87,14 +86,22 @@ EXPORT uint8_t calcCRC(const uint8_t *buf, size_t size)
     for (uint8_t j = 0; j < size; j++) {
         uint8_t i = buf[j] ^ crc;
         crc = 0;
-        if (i & 0x01) crc ^= 0x5e;
-        if (i & 0x02) crc ^= 0xbc;
-        if (i & 0x04) crc ^= 0x61;
-        if (i & 0x08) crc ^= 0xc2;
-        if (i & 0x10) crc ^= 0x9d;
-        if (i & 0x20) crc ^= 0x23;
-        if (i & 0x40) crc ^= 0x46;
-        if (i & 0x80) crc ^= 0x8c;
+        if (i & 0x01)
+            crc ^= 0x5e;
+        if (i & 0x02)
+            crc ^= 0xbc;
+        if (i & 0x04)
+            crc ^= 0x61;
+        if (i & 0x08)
+            crc ^= 0xc2;
+        if (i & 0x10)
+            crc ^= 0x9d;
+        if (i & 0x20)
+            crc ^= 0x23;
+        if (i & 0x40)
+            crc ^= 0x46;
+        if (i & 0x80)
+            crc ^= 0x8c;
     }
     return crc % 256;
 }
@@ -116,12 +123,12 @@ void turn_on_all_pumps()
 void turn_on_pump_1()
 {
     if (time_ms() > startTimerPumpON + DELAY_PUMP * 1000) {
-        if ((bool)m_pump2::value() || (bool)m_pump3::value()) {
+        if ((bool) m_pump2::value() || (bool) m_pump3::value()) {
             m_pump2::publish(0u);
             m_pump3::publish(0u);
             return;
         }
-        if (!(bool)m_pump1::value()) {
+        if (!(bool) m_pump1::value()) {
             m_pump1::publish(1u);
             startTimerPumpON = time_ms();
         }
@@ -131,12 +138,12 @@ void turn_on_pump_1()
 void turn_on_pump_2()
 {
     if (time_ms() > startTimerPumpON + DELAY_PUMP * 1000) {
-        if ((bool)m_pump1::value() || (bool)m_pump3::value()) {
+        if ((bool) m_pump1::value() || (bool) m_pump3::value()) {
             m_pump1::publish(0u);
             m_pump3::publish(0u);
             return;
         }
-        if (!(bool)m_pump2::value()) {
+        if (!(bool) m_pump2::value()) {
             m_pump2::publish(1u);
             startTimerPumpON = time_ms();
         }
@@ -146,12 +153,12 @@ void turn_on_pump_2()
 void turn_on_pump_3()
 {
     if (time_ms() > startTimerPumpON + DELAY_PUMP * 1000) {
-        if ((bool)m_pump1::value() || (bool)m_pump2::value()) {
+        if ((bool) m_pump1::value() || (bool) m_pump2::value()) {
             m_pump1::publish(0u);
             m_pump2::publish(0u);
             return;
         }
-        if (!(bool)m_pump3::value()) {
+        if (!(bool) m_pump3::value()) {
             m_pump3::publish(1u);
             startTimerPumpON = time_ms();
         }
@@ -224,16 +231,24 @@ void pump_stage_4() //get fuel from tank 1 and 2 until empty
 }
 
 void fuel_auto_control()
-{   
-    if(m_thr::value() > 0.85f) {
+{
+    if (m_thr::value() > 0.85f) {
         turn_on_all_pumps();
         return;
     }
     switch (pump_stage) {
-        case 1: pump_stage_1(); break;
-        case 2: pump_stage_2(); break;
-        case 3: pump_stage_3(); break;
-        case 4: pump_stage_4(); break;
+    case 1:
+        pump_stage_1();
+        break;
+    case 2:
+        pump_stage_2();
+        break;
+    case 3:
+        pump_stage_3();
+        break;
+    case 4:
+        pump_stage_4();
+        break;
     }
 }
 
@@ -248,9 +263,9 @@ void check_answer_time()
 
 void calc_flowrate()
 {
-    if((bool)m_ignition::value()) {
+    if ((bool) m_ignition::value()) {
         ignitionTotalTime += FUEL_TASK_PERIOD;
-        float flowrate = ( 100.0f - M_FUEL_P::value() ) / ((float)ignitionTotalTime / 3600000.0f);
+        float flowrate = (100.0f - M_FUEL_P::value()) / ((float) ignitionTotalTime / 3600000.0f);
         m_flowrate::publish(flowrate);
     }
 }
@@ -262,15 +277,15 @@ EXPORT void on_serial(const uint8_t *data, size_t size)
             if (data[8] == calcCRC(data, 8)) {
                 if (data[1] == ADR_SENS1) {
                     m_timeAns1 = time_ms();
-                    m_vFuelPercent1 = (float)(data[4] | (data[5] << 8)) / 10.0f;
+                    m_vFuelPercent1 = (float) (data[4] | (data[5] << 8)) / 10.0f;
                     M_FUEL_V1::publish(m_vFuelPercent1 * V_MAX1 / 100.0f);
                 } else if (data[1] == ADR_SENS2) {
                     m_timeAns2 = time_ms();
-                    m_vFuelPercent2 = (float)(data[4] | (data[5] << 8)) / 10.0f;
+                    m_vFuelPercent2 = (float) (data[4] | (data[5] << 8)) / 10.0f;
                     M_FUEL_V2::publish(m_vFuelPercent2 * V_MAX2 / 100.0f);
                 } else if (data[1] == ADR_SENS3) {
                     m_timeAns3 = time_ms();
-                    m_vFuelPercent3 = (float)(data[4] | (data[5] << 8)) / 10.0f;
+                    m_vFuelPercent3 = (float) (data[4] | (data[5] << 8)) / 10.0f;
                     M_FUEL_V3::publish(m_vFuelPercent3 * V_MAX3 / 100.0f);
                 }
             }
@@ -280,7 +295,6 @@ EXPORT void on_serial(const uint8_t *data, size_t size)
 
 EXPORT void on_task()
 {
-
 #ifdef DEBUG
     // Debug fuel simulation code
     float ctr_thr = m_thr::value();
@@ -312,13 +326,11 @@ EXPORT void on_task()
         M_FUEL_V3::publish(new_val);
     }
 
-    if (m_vFuelPercent3 < 100.0f && 
-        (m_pump1::value() != 0 || m_pump2::value() != 0 || m_pump3::value() != 0)) {
+    if (m_vFuelPercent3 < 100.0f && (m_pump1::value() != 0 || m_pump2::value() != 0 || m_pump3::value() != 0)) {
         float new_val = M_FUEL_V3::value() + 0.01f * ctr_thr;
         M_FUEL_V3::publish(new_val);
     }
-#endif    
-
+#endif
 
     check_answer_time();
     calc_flowrate();
@@ -334,12 +346,11 @@ EXPORT void on_task()
     m_vFuelPercent3 = fl3 * 100 / V_MAX3; //for debug only
 #endif
 
-    M_FUEL_P::publish((m_vFuelPercent1 + m_vFuelPercent2 + m_vFuelPercent3)/3);
+    M_FUEL_P::publish((m_vFuelPercent1 + m_vFuelPercent2 + m_vFuelPercent3) / 3);
 
-
-    ignition = (bool)m_ignition::value();
-    algorithm = (bool)m_algorithm::value();
-    ers = (bool)m_ers::value();
+    ignition = (bool) m_ignition::value();
+    algorithm = (bool) m_algorithm::value();
+    ers = (bool) m_ers::value();
 
     if (ers) {
         turn_off_all_pumps();
@@ -355,7 +366,7 @@ EXPORT void on_task()
             algorithmOld = algorithm;
             if (algorithm) {
                 printf("manual fuel ON");
-                
+
             } else {
                 turn_off_all_pumps();
                 printf("manual fuel OFF");
