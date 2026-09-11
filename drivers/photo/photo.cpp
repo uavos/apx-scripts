b837776719ac508bd53e8cad032fd510ffb7e1c9
Eugene Shilin <se@uavos.com>
2026-09-11 11:22:45 +0100

    check delay increased to 200ms

--- drivers/photo/photo.cpp
+++ drivers/photo/photo.cpp
@@ -2,7 +2,7 @@
 #include <apx.h>
 
 constexpr const uint8_t TASK_MAIN_MS{10};
-constexpr const uint16_t CHECK_DELAY_MS{60};
+constexpr const uint16_t CHECK_DELAY_MS{200};
 
 //inputs
 using CAM_RELEASE = Mandala<mandala::ctr::env::cam::shot>;
